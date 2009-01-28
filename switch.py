d228ee870c3752b9226f8bf282997567bf446c82
stefan.pouwels@gmail.com <stefan.pouwels@gmail.com@f298be44-ebec-11dd-9ba5-89a75009fd5d>
2009-01-28 18:15:46 +0000

    1st testversion

---
+++ switch.py
@@ -0,0 +1,28 @@
+import cgi
+import os
+import re
+import Cookie
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+
+    target = 'index.html'            
+    template_values = {
+      'test': 'hello world'
+      }
+
+    path = os.path.join(os.path.dirname(__file__), target)
+    self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([('/', MainPage)],debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
